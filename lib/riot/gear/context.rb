9c3d25ab5943cffefdb15462c95618be62a0370b
Justin Knowlden <gus@gusg.us>
2010-07-12 12:55:35 -0500

    Argh!

--- lib/riot/gear/context.rb
+++ lib/riot/gear/context.rb
@@ -1,4 +1,4 @@
-require 'riot/gear/context/http'
+require 'riot/gear/context/persist_cookie'
 require 'riot/gear/context/asserts_status'
 require 'riot/gear/context/asserts_header'
 require 'riot/gear/context/asserts_json'
